034dc70492dbe7ec7fac45281a35e2b6ad7fcb00
Vince Suriani <chenzorama@hotmail.com>
2022-02-06 11:32:13 -0500

    will this work now?

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['github.com', 'chenzorama.com', 'cdn.discordapp.com'],
+  },
   assetPrefix: isProd ? '/sherpa-sot/' : ''
 }
