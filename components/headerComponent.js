918dd0e395f72579b5caae27396ddf647424139c
Vince Suriani <chenzorama@hotmail.com>
2022-02-15 08:01:28 -0500

    theming

---
+++ components/headerComponent.js
@@ -0,0 +1,17 @@
+import styles from './headerComponent.module.scss'
+
+export default function Header() {
+
+    return (
+
+        <header className={styles.header}>
+
+            <div className={styles.container}>
+            Keelhauled Podcast Discord Sherpa Program
+            </div>
+
+        </header>
+
+
+    )
+}
