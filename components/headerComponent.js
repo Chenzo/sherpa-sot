0efa4f16e5be5e36013ca85977a8351fffb00ff2
Chenzo <chenzorama@hotmail.com>
2022-04-03 16:22:14 -0400

    Ok, this should be more

--- components/headerComponent.js
+++ components/headerComponent.js
@@ -1,4 +1,6 @@
 import styles from './headerComponent.module.scss'
+import Link from 'next/link'
+
 
 export default function Header() {
 
@@ -7,7 +9,11 @@ export default function Header() {
         <header className={styles.header}>
 
             <div className={styles.container}>
-            Keelhauled Podcast Discord Sherpa Program
+            <Link href="/"><a> Sherpa Knowledge Repository</a></Link>
+
+            <ul>
+                <li><Link href="/things-to-do"><a>List of Things to Do</a></Link></li>
+            </ul>
             </div>
 
         </header>
